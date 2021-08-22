b0d3659f77c7ed9c42bea50253c3799d25b7ebbc
noora <noorayaqub44@gmail.com>
2021-08-22 15:41:29 +0300

    commit tree 56d07bc04a35e32e9b40feb91630da5f8931b1e6

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,25 +1,32 @@
-import React, { useState } from "react";
+import React from "react";
 import Item from "./Item";
 
-function ShoppingList({ items }) {
-  const [selectedCategory, setSelectedCategory] = useState("All");
 
-  function handleCategoryChange(event) {
-    // event.target.value will be the value selected by the user
-    setSelectedCategory(event.target.value);
+class ShoppingList extends React.Component {
+  
+  state ={selectedCategory:"All"};
+
+  handleCategoryChange = (event) => {
+    this.setState({ selectedCategory: event.target.value });
+  };
+
+  getItemsToDisplay() {
+    return this.props.items
+      .filter(
+        (item) =>
+          this.state.selectedCategory === "All" ||
+          item.category === this.state.selectedCategory
+      )
+      .map((item) => (
+        <Item key={item.id} name={item.name} category={item.category} />
+      ));
   }
-
-  // we want to filter the items to only display the ones based on the selected category
-  const itemsToDisplay = items.filter((item) => {
-    if (selectedCategory === "All") return true;
-
-    return item.category === selectedCategory;
-  });
-
+ render(){
+  
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter" onChange={handleCategoryChange}>
+        <select name="filter" onChange={this.handleCategoryChange}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -27,12 +34,11 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {itemsToDisplay.map((item) => (
-          <Item key={item.id} name={item.name} category={item.category} />
-        ))}
+        {this.getItemsToDisplay()}
       </ul>
     </div>
   );
+ }
 }
 
 export default ShoppingList;
